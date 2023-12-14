205787381f56c0ea3d5ea2ba39455e101cecd4d6
yangmengbo <yangmengbo22@csu.ac.cn>
2023-12-14 16:43:02 +0800

    chore: 添加版权信息

--- src/components/copyright.tsx
+++ src/components/copyright.tsx
@@ -1,11 +1,19 @@
 import { Typography } from 'antd'
 import { BUILD_TIME } from '../constant.ts'
+import { name, version } from '../../package.json'
 import React from 'react'
 
 export const Copyright: React.FC = () => {
   return (
     <div className={'mx-auto w-fit text-center py-8'}>
-      <Typography.Text type={'secondary'}>更新于 {new Date(BUILD_TIME).toString()}</Typography.Text>
+      <div>
+        <Typography.Text strong>
+          {name} v{version}
+        </Typography.Text>
+      </div>
+      <div>
+        <Typography.Text type={'secondary'}>构建于 {new Date(BUILD_TIME).toString()}</Typography.Text>
+      </div>
     </div>
   )
 }
